4f809d0c04aec346fa41764b5b8e987f81cc6698
folalekan13@yahoo.com <folalekan13@yahoo.com>
2020-04-03 21:56:14 +0100

    initial commit

--- app.js
+++ app.js
@@ -1,51 +1,51 @@
-// const data = [
-//   {
-//     principal: 2500,
-//     time: 1.8
-//   },
-//   {
-//     principal: 1000,
-//     time: 5
-//   },
-//   {
-//     principal: 3000,
-//     time: 1
-//   },
-//   {
-//     principal: 2000,
-//     time: 3
-//   }
-// ];
+const data = [
+  {
+    principal: 2500,
+    time: 1.8
+  },
+  {
+    principal: 1000,
+    time: 5
+  },
+  {
+    principal: 3000,
+    time: 1
+  },
+  {
+    principal: 2000,
+    time: 3
+  }
+];
 
-// function interestCalculator(arr) {
-//   // Set rate
-//   arr.forEach(el => {
-//     const { principal, time } = el;
+function interestCalculator(arr) {
+  // showing rate for each
+  arr.forEach(el => {
+    const { principal, time } = el;
 
-//     if (principal >= 2500 && time > 1 && time < 3) {
-//       el.rate = 3;
-//     } else if (principal >= 2500 && time >= 3) {
-//       el.rate = 4;
-//     } else if (principal < 2500 || time <= 1) {
-//       el.rate = 2;
-//     } else {
-//       el.rate = 1;
-//     }
-//   });
+    if (principal >= 2500 && time > 1 && time < 3) {
+      el.rate = 3;
+    } else if (principal >= 2500 && time >= 3) {
+      el.rate = 4;
+    } else if (principal < 2500 || time <= 1) {
+      el.rate = 2;
+    } else {
+      el.rate = 1;
+    }
+  });
 
-//   // Calculate interest
+  // caculating the interest
 
-//   const interestData = arr.map(el => {
-//     const { principal, time, rate } = el;
+  const interestData = arr.map(el => {
+    const { principal, time, rate } = el;
 
-//     el.interest = (principal * rate * time) / 100;
+    el.interest = (principal * rate * time) / 100;
 
-//     return el;
-//   });
+    return el;
+  });
 
-//   console.log(interestData);
+  console.log(interestData);
 
-//   return interestData;
-// }
+  return interestData;
+}
 
-// interestCalculator(data);
+interestCalculator(data);
